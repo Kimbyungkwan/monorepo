5465d06f1efaf8541296ce96d309b10a403ffc06
Kimbyungkwan <kqudk@naver.com>
2022-11-20 10:33:57 +0900

    chore(@mono/server-1): @mono/utils

--- packages/mono-server-1/app.ts
+++ packages/mono-server-1/app.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
-
+import { uuidV4 } from '@mono/utils/src';
 const PORT = 8080;
 const app = express();
 
 app.use(express.json());
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('welcome!');
+});
+app.get('/uuid', (req: Request, res: Response, next: NextFunction) => {
+  res.send(uuidV4());
 });
 
 app.listen(PORT, () => {
