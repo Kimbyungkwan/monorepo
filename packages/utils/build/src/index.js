c3a70c83eda7f409221e0298163bfe44aec43e45
Kimbyungkwan <kqudk@naver.com>
2022-11-20 10:31:52 +0900

    chore(@mono/utils): packages.json

---
+++ packages/utils/build/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.uuidV4 = void 0;
+var uuidv4_1 = require("./uuidv4");
+Object.defineProperty(exports, "uuidV4", { enumerable: true, get: function () { return __importDefault(uuidv4_1).default; } });
+//# sourceMappingURL=index.js.map
